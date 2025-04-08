d9470a39eb7beadfeb38199c0084e0a46ab2cb66
Chiller Cat <81083840+Eternal587@users.noreply.github.com>
2025-04-08 14:28:54 -0400

    Update Unscripted.js

--- Unscripted.js
+++ Unscripted.js
@@ -8,7 +8,7 @@ if (choice === "1") {
   win.document.write("<embed src='https://www.google.com/webhp?igu=1' style='width: 100%; height: 100%; top: 0; left: 0; position: fixed;'></embed>")
 }
 if (choice === "2") {
-  win.document.write('<body style="background-color: rgb(35, 35, 135, 1)"></body> <button onclick="newUnexTab()">Create Injected UNEX Tab</button> <script>function newUnexTab() { let x = prompt("Insert Code"); let y = window.open("google.com"); y.document.write(x); }</script>')
+  win.document.write('<body style="background-color: rgb(35, 35, 135, 1)"></body> <button onclick="newUnexTab()">Create Injected UNEX Tab</button> <script>function newUnexTab() { let x = prompt("Insert Code"); let y = window.open("google.com","","width=500, height=500"); y.document.write(x); }</script>')
 }
 if (choice === "3") {
   win.document.write('<embed src="https://eternal587.github.io/TheCloset/Unbrowser.html" style="width: 100%; height: 100%; top: 0; left: 0; position: fixed;"></embed>')
